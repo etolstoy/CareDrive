88737e8c2e4fca1adccced122f12b07989f3807b
Egor Tolstoy <igrekde@gmail.com>
2015-04-12 16:01:29 +0300

    Отрефакторен DRVCountryGeoService

--- CareDrive/DRVCountryGeoServiceBase.m
+++ CareDrive/DRVCountryGeoServiceBase.m
@@ -8,49 +8,97 @@
 
 #import "DRVCountryGeoServiceBase.h"
 #import "DRVCountryGeoData.h"
+#import "DRVGeoJsonConstants.h"
 
 #import <CoreLocation/CoreLocation.h>
 
+static NSString *const geojsonName = @"world-110m";
+static NSString *const geojsonType = @"geojson";
+
+@interface DRVCountryGeoServiceBase ()
+
+@property (strong, nonatomic) NSDictionary *countryFeatures;
+
+@end
+
 @implementation DRVCountryGeoServiceBase
 
-- (DRVCountryGeoData *)geoDataForCountryWithCountryCode:(NSString *)countryCode {
-    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:@"world-110m" ofType:@"geojson"];
-    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[[NSData alloc] initWithContentsOfFile:jsonPath] options:0 error:nil];
-    NSDictionary *features = json[@"features"];
-    for (NSDictionary *feature in features) {
-        NSString *iso = feature[@"properties"][@"iso_a3"];
-        if ([iso isEqualToString:countryCode]) {
-            NSDictionary *geometry = feature[@"geometry"];
-            NSString *polygonType = geometry[@"type"];
-            NSMutableArray *polygonsArray = [@[] mutableCopy];
-            
-            if ([polygonType isEqualToString:@"Polygon"]) {
-                NSMutableArray *locationsArray = [@[] mutableCopy];
-                NSArray *coordinates = [geometry[@"coordinates"] firstObject];
-                for (NSArray *coordinate in coordinates) {
-                    CLLocation *location = [[CLLocation alloc] initWithLatitude:[[coordinate lastObject] doubleValue] longitude:[[coordinate firstObject] doubleValue]];
-                    [locationsArray addObject:location];
-                }
-                [polygonsArray addObject:[locationsArray copy]];
-            } else if ([polygonType isEqualToString:@"MultiPolygon"]) {
-                NSArray *polygons = geometry[@"coordinates"];
-                for (NSArray *polygon in polygons) {
-                    NSMutableArray *locationsArray = [@[] mutableCopy];
-                    NSArray *coordinates = [polygon firstObject];
-                    for (NSArray *coordinate in coordinates) {
-                        CLLocation *location = [[CLLocation alloc] initWithLatitude:[[coordinate lastObject] doubleValue] longitude:[[coordinate firstObject] doubleValue]];
-                        [locationsArray addObject:location];
-                    }
-                    [polygonsArray addObject:[locationsArray copy]];
-                }
-            }
-            
-            DRVCountryGeoData *geoData = [DRVCountryGeoData countryGeoDataWithCountryCode:countryCode polygons:[polygonsArray copy]];
-            return geoData;
-        }
+- (instancetype)init {
+    if (self = [super init]) {
+        [self setupCountriesDictionary];
     }
+    return self;
+}
 
-    return nil;
+- (DRVCountryGeoData *)geoDataForCountryWithCountryCode:(NSString *)countryCode {
+    NSDictionary *feature = self.countryFeatures[countryCode];
+    NSDictionary *geometry = feature[DRVGeoJsonGeometryKey];
+    NSString *polygonType = geometry[DRVGeoJsonTypeKey];
+    NSMutableArray *polygonsArray = [@[] mutableCopy];
+    
+    NSDictionary *polygonTypeSelectorsDictionary = [self polygonTypeSelectorsDictionary];
+    SEL processingSelector = NSSelectorFromString(polygonTypeSelectorsDictionary[polygonType]);
+    polygonsArray = [self performSelector:processingSelector withObject:geometry];
+    
+    DRVCountryGeoData *geoData = [DRVCountryGeoData countryGeoDataWithCountryCode:countryCode polygons:[polygonsArray copy]];
+    return geoData;
+}
+
+#pragma mark - Приватные методы
+
+- (NSArray *)processPolygonTypeWithGeometryDictionary:(NSDictionary *)geometryDictionary {
+    NSMutableArray *polygonsArray = [@[] mutableCopy];
+    
+    NSArray *coordinates = [geometryDictionary[DRVGeoJsonCoordinatesKey] firstObject];
+    NSArray *polygon = [self locationsArrayForCoordinatesArray:coordinates];
+    [polygonsArray addObject:polygon];
+    
+    return [polygonsArray copy];
+}
+
+- (NSArray *)processMultiPolygonTypeWithGeometryDictionary:(NSDictionary *)geometryDictionary {
+    NSMutableArray *polygonsArray = [@[] mutableCopy];
+    
+    NSArray *geometries = geometryDictionary[DRVGeoJsonCoordinatesKey];
+    for (NSArray *geometry in geometries) {
+        NSArray *coordinates = [geometry firstObject];
+        NSArray *polygon = [self locationsArrayForCoordinatesArray:coordinates];
+        [polygonsArray addObject:polygon];
+    }
+    
+    return [polygonsArray copy];
+}
+
+- (NSArray *)locationsArrayForCoordinatesArray:(NSArray *)coordinatesArray {
+    NSMutableArray *locationsMutableArray = [@[] mutableCopy];
+    for (NSArray *coordinate in coordinatesArray) {
+        CGFloat latitude = [[coordinate lastObject] doubleValue];
+        CGFloat longitude = [[coordinate firstObject] doubleValue];
+        
+        CLLocation *location = [[CLLocation alloc] initWithLatitude:latitude longitude:longitude];
+        [locationsMutableArray addObject:location];
+    }
+    return [locationsMutableArray copy];
+}
+
+- (NSDictionary *)polygonTypeSelectorsDictionary {
+    return @{
+             DRVGeoJsonPolygonTypeKey : NSStringFromSelector(@selector(processPolygonTypeWithGeometryDictionary:)),
+             DRVGeoJsonMultiPolygonTypeKey : NSStringFromSelector(@selector(processMultiPolygonTypeWithGeometryDictionary:))
+             };
+}
+
+- (void)setupCountriesDictionary {
+    NSString *jsonPath = [[NSBundle mainBundle] pathForResource:geojsonName ofType:geojsonType];
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:[[NSData alloc] initWithContentsOfFile:jsonPath] options:0 error:nil];
+    
+    NSMutableDictionary *mutableCountryFeatures = [@{} mutableCopy];
+    NSDictionary *features = json[DRVGeoJsonFeaturesKey];
+    for (NSDictionary *feature in features) {
+        NSString *countryCode = feature[DRVGeoJsonPropertiesKey][DRVGeoJsonISOA3Key];
+        [mutableCountryFeatures setObject:feature forKey:countryCode];
+    }
+    self.countryFeatures = [mutableCountryFeatures copy];
 }
 
 @end
---
+++ CareDrive/DRVGeoJsonConstants.m
@@ -0,0 +1,18 @@
+//
+//  DRVGeoJsonConstants.m
+//  CareDrive
+//
+//  Created by Egor Tolstoy on 12.04.15.
+//  Copyright (c) 2015 Egor Tolstoy. All rights reserved.
+//
+
+#import "DRVGeoJsonConstants.h"
+
+NSString *const DRVGeoJsonGeometryKey = @"geometry";
+NSString *const DRVGeoJsonTypeKey = @"type";
+NSString *const DRVGeoJsonCoordinatesKey = @"coordinates";
+NSString *const DRVGeoJsonFeaturesKey = @"features";
+NSString *const DRVGeoJsonPropertiesKey = @"properties";
+NSString *const DRVGeoJsonISOA3Key = @"iso_a3";
+NSString *const DRVGeoJsonPolygonTypeKey = @"Polygon";
+NSString *const DRVGeoJsonMultiPolygonTypeKey = @"MultiPolygon";
